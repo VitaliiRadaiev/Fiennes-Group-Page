4a5a3da8d979fc78f01df65204d53fbea6fa9e1b
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-10-22 12:53:11 +0300

    add page

--- js/main.js
+++ js/main.js
@@ -287,6 +287,36 @@ cardJournalHandler();;
 }
 // == and  slider ==========================================================================
 ;
+{
+	let slider = document.querySelector('.line-slider .swiper-container');
+	if(slider) {
+
+		var mySwiper = new Swiper(slider, {
+		slidesPerView:3,
+
+		speed: 600,
+		spaceBetween: 30,
+		pagination: {
+		    el: slider.querySelector('.swiper-pagination'),
+		    clickable: true,
+		  },
+		 breakpoints: {
+		 	320: {
+		 		spaceBetween: 10,
+		 		slidesPerView:1,
+		 	},
+		 	576: {
+		 		spaceBetween: 10,
+		 		slidesPerView:2,
+		 	},
+		   768: {
+		    spaceBetween: 30,
+		    slidesPerView:3,
+		   }
+		 } 
+		})
+	}
+};
 // === COMMON ==================================================================
 
 
